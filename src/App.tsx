8b8f8c1b8427650593414e01ee7af92fae3494e7
stephen <u6269649@anu.edu.au>
2023-07-23 15:58:23 +1000

    BUILD: AuthContext with routes separation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,37 @@ import Login from "@/components/Login";
 import Logout from "@/components/Logout";
 import Protected from "@/components/Protected";
 import Unprotected from "@/components/Unprotected";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AuthProvider, {
+  AuthIsNotSignedIn,
+  AuthIsSignedIn,
+} from "@/contexts/AuthContext";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={"/login"} element={<Login />} />
-        <Route path={"/logout"} element={<Logout />} />
-        <Route path={"/protected"} element={<Protected />} />
-        <Route path={"/unprotected"} element={<Unprotected />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <AuthIsSignedIn>
+        <Router>
+          <Routes>
+            <Route path={"/logout"} element={<Logout />} />
+            <Route path={"/protected"} element={<Protected />} />
+          </Routes>
+        </Router>
+      </AuthIsSignedIn>
+      <AuthIsNotSignedIn>
+        <Router>
+          <Routes>
+            <Route path={"/login"} element={<Login />} />
+            <Route path={"/unprotected"} element={<Unprotected />} />
+            <Route path="/*" element={<Navigate replace to={"/login"} />} />
+          </Routes>
+        </Router>
+      </AuthIsNotSignedIn>
+    </AuthProvider>
   );
 }
